43f1ba3ff5319ab293e63b0b6a52338a20f6cdae
Albertus Chua <albertus.chua@gmail.com>
2022-06-09 17:44:26 -0400

    Added more character entries

--- server.js
+++ server.js
@@ -6,40 +6,50 @@ const PORT = 8000;
 app.use(cors());
 
 const characters = {
-    'michael': {
+    'michael bluth': {
         'name': 'Michael Bluth',
         'occupation': 'Bluth Company CEO',
         'quote': 'I don\'t know what I expected.'
     },
-    'gob': {
+    'gob bluth': {
         'name': 'George Oscar Bluth Jr.',
         'occupation': 'Magician',
         'quote': 'I\'ve made a huge mistake.'
     },
-    'lindsay': {
+    'lindsay bluth': {
         'name': 'Lindsay Bluth Funke, (Formerly: Nellie)',
         'occupation': 'Activist',
         'quote': 'Michael, it was shoplifting, and I\'m white. I think I\'m going to be ok.'
     },
-    'buster': {
+    'buster bluth': {
         'name': 'Buster Bluth',
         'occupation': 'Army soldier',
         'quote': 'I\'M A MONSTER!!!!'
     },
-    'tobias': {
+    'tobias funke': {
         'name': 'Tobias Funke',
         'occupation': 'Anal-rapist, actor',
-        'quote': 'I think I just blue myself.'
+        'quote': 'I just blue myself.'
     },
-    'george': {
+    'george bluth': {
         'name': 'George Bluth Sr.',
         'occupation': 'Former CEO of the Bluth Company',
         'quote': 'There\'s always money in the banana stand.'
     },
-    'lucille': {
+    'lucille bluth': {
         'name': 'Lucille Bluth',
         'occupation': 'None',
         'quote': 'I mean it\'s one banana, Michael. What could it cost? 10 dollars?'
+    },
+    'george michael bluth': {
+        'name': 'George Michael Bluth',
+        'occupation': 'Banana Stand worker',
+        'quote': "Okay, it was for me. Yea. I was gonna smoke the marijuana like a cigarette."
+    },
+    'maeby funke': {
+        'name': 'Maeby Funke',
+        'occupation': 'Accidental Studio Executive',
+        'quote': "Marry me!"
     },
     'unknown': {
         'name': 'unknown',
@@ -48,6 +58,7 @@ const characters = {
     }
 }
 
+// API search 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
@@ -60,6 +71,6 @@ app.get('/api/:name', (req, res) => {
     }
 })
 
-app.listen(process.env.PORT || PORT, (req, res) => {
-    console.log(`The server is runnong on ${PORT}.`);
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is running on ${PORT}.`);
 })
